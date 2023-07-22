c504b4fd45fd9473bd326215880f234d50ca767b
lee <lee@local>
2023-07-22 20:30:48 +0800

    Add limit to notifications

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -31,7 +31,7 @@ export default function App({ Component, pageProps }: AppProps) {
             }
           }}
           withGlobalStyles withNormalizeCSS>
-          <Notifications />
+          <Notifications limit={5}/>
           <AppLayout>
             <Component {...pageProps} />
           </AppLayout>
