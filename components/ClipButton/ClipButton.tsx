fc257845c85c5327b601607eefbfd06d1683b901
lee <lee@local>
2023-07-23 19:15:30 +0800

    Update ui

--- components/ClipButton/ClipButton.tsx
+++ components/ClipButton/ClipButton.tsx
@@ -1,18 +1,19 @@
-import { Button } from '@mantine/core';
+import { Button, ButtonProps } from '@mantine/core';
 import { useClipboard } from '@mantine/hooks';
 
-interface ClipButtonProps {
+interface ClipButtonProps extends ButtonProps {
   value: string;
   hint?: string;
 };
 
-export default function ClipButton({ value, hint }: ClipButtonProps) {
+export default function ClipButton({ value, hint, disabled }: ClipButtonProps) {
   const clipboard = useClipboard({ timeout: 500 });
 
   return (
     <Button
       color={clipboard.copied ? 'teal' : 'blue'}
       onClick={() => clipboard.copy(value)}
+      disabled={disabled}
     >
       {clipboard.copied ? '已复制!' : (hint || '复制')}
     </Button>
--- pages/moelist.tsx
+++ pages/moelist.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import Head from 'next/head';
-import { Container, Group } from '@mantine/core';
+import { Container, Group, Select } from '@mantine/core';
 import { notifications } from '@mantine/notifications';
 
 import useMountEffectOnce from '@/hooks/useMountEffectOnce';
@@ -26,20 +26,18 @@ export default function Moelist() {
   const onDrop = async (files: File[]) => {
     if (files.length === 0) return;
 
-    Promise.allSettled(files.map(ArchiveInfoReader.open))
-      .then((results) => {
-        let infos = [];
-        for (let result of results) {
-          if (result.status === "fulfilled") {
-            infos.push(result.value);
-          } else {
-            showError(result.reason);
-          }
-        }
-        if (infos.length > 0) {
-          setArchiveInfos(infos);
-        }
-      });
+    let infos = [];
+    let results = await Promise.allSettled(ArchiveInfoReader.open)
+    for (let result of results) {
+      if (result.status === "fulfilled") {
+        infos.push(result.value);
+      } else {
+        showError(result.reason);
+      }
+    }
+    if (infos.length > 0) {
+      setArchiveInfos(infos);
+    }
   }
 
   return (
@@ -48,21 +46,34 @@ export default function Moelist() {
         <FilePicker
           onDrop={onDrop}
         />
-        {archiveInfos.length > 0 &&(
-          <>
-            <Group position='center' py={10}>
-              <ClipButton
-                value={MoelistFormatter.getCodeStyle(archiveInfos)}
-                hint='代码'
-              />
-              <ClipButton
-                value={MoelistFormatter.getTableStyle(archiveInfos)}
-                hint='表格'
-              />
-            </Group>
-            <pre> {MoelistFormatter.getPreviewStyle(archiveInfos)} </pre>
-          </>
-        )}
+        <Group position='center' py={20}>
+          <Select
+            clearable
+            placeholder="版块 (Preview)"
+            data={[
+              { value: 'A', label: '非单行本分享区' },
+              { value: 'B', label: '自制漫画分享区' },
+              { value: 'C', label: '原创内部分享区' },
+              { value: 'D', label: '实体首发补档区' },
+              { value: 'E', label: '实体二次分流区'},
+              { value: 'F', label: '繁体中文电子版' },
+              { value: 'G', label: '简体中文电子版'}
+            ]}
+          />
+          <ClipButton
+            disabled={archiveInfos.length === 0}
+            value={MoelistFormatter.getCodeStyle(archiveInfos)}
+            hint='代码'
+          />
+          <ClipButton
+            disabled={archiveInfos.length === 0}
+            value={MoelistFormatter.getTableStyle(archiveInfos)}
+            hint='表格'
+          />
+        </Group>
+        {archiveInfos.length > 0 && 
+          <pre> {MoelistFormatter.getPreviewStyle(archiveInfos)} </pre>
+        }
       </Container>
     </>
   )
