4d2b076093ab298dc8761eb13d0c690789025b5f
lee <lee@local>
2023-07-27 15:23:58 +0800

    delete navbar

--- components/AppLayout/AppLayout.tsx
+++ components/AppLayout/AppLayout.tsx
@@ -1,11 +1,8 @@
-import { useState } from 'react';
 import Link from 'next/link';
-import { useRouter } from 'next/router';
-import { ActionIcon, AppShell, Burger, Group, Header, MediaQuery, Navbar, NavLink, ScrollArea, Text } from '@mantine/core';
+import { ActionIcon, AppShell, Box, Group, Header, Text } from '@mantine/core';
 
 import { IconBrandGithub } from '@tabler/icons-react';
 import ColorSchemeToggle from '@/components/ColorSchemeToggle/ColorSchemeToggle';
-import { navlinkItems } from './navlink-items';
 
 interface AppLayoutProps {
   children: React.ReactNode;
@@ -13,43 +10,10 @@ interface AppLayoutProps {
 };
 
 export default function AppLayout({ children, title }: AppLayoutProps) {
-  const router = useRouter();
-  const [opened, setOpened] = useState(false);
-
-  const list = navlinkItems.map((item) => (
-    <NavLink
-      key={item.label}
-      label={item.label}
-      active={router.pathname === item.href}
-      component={Link}
-      href={item.href}
-      variant="light"
-      sx={(theme) => ({
-        borderRadius: theme.radius.md,
-        fontWeight: 500,
-      })}
-    />
-  ));
-
-  const navbar = (
-    <Navbar hiddenBreakpoint="sm" hidden={!opened} width={{ sm: 300 }} p="xs">
-      <Navbar.Section grow component={ScrollArea}>
-        {list}
-      </Navbar.Section>
-    </Navbar>
-  );
-
   const header = (
     <Header height={{ base: 50 }} p="xs">
       <Group position="apart">
-        <MediaQuery largerThan="sm" styles={{ visibility: 'hidden' }}>
-          <Burger
-            opened={opened}
-            onClick={() => setOpened((o) => !o)}
-            size="sm"
-            mr="xl"
-          />
-        </MediaQuery>
+        <Box/>
         <Text fw={700}> { title || 'Toolbox'} </Text>
         <Group>
           <ActionIcon variant="default" radius="xl" component={Link} href="/">
@@ -63,8 +27,6 @@ export default function AppLayout({ children, title }: AppLayoutProps) {
 
   return (
     <AppShell
-      navbarOffsetBreakpoint="sm"
-      // navbar={navbar}
       header={header}
     >
       {children}
