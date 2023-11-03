461e1e580b11fa426cfcf72e37337b6846b6ea8f
DearRabbit <DearRabbit@users.noreply.github.com>
2023-11-03 11:33:59 +0800

    fix precedence

--- scripts/patch-unrar.js
+++ scripts/patch-unrar.js
@@ -4,6 +4,6 @@ const unrarFile = "node_modules/node-unrar-js/dist/js/unrar.js";
 
 if (fs.existsSync(unrarFile)) {
   let content = fs.readFileSync(unrarFile, "utf8");
-  content = content.replace("offsetLo+offsetHi*4294967296", "Number(BigInt(offsetLo) & 0xffffffffn + BigInt(offsetHi) * 0x100000000n)");
+  content = content.replace("offsetLo+offsetHi*4294967296", "Number((BigInt(offsetLo) & 0xffffffffn) + (BigInt(offsetHi) * 0x100000000n))");
   fs.writeFileSync(unrarFile, content);
 }
