1ea9331f49c2ab12bc3fbca7da9fe43ee4f66c64
DearRabbit <DearRabbit@users.noreply.github.com>
2024-04-11 18:27:55 +0800

    bump node-unrar-js from 2.0.0 to 2.0.2

--- scripts/patch-unrar.js
+++
@@ -1,9 +0,0 @@
-const fs = require('node:fs');
-
-const unrarFile = "node_modules/node-unrar-js/dist/js/unrar.js";
-
-if (fs.existsSync(unrarFile)) {
-  let content = fs.readFileSync(unrarFile, "utf8");
-  content = content.replace("offsetLo+offsetHi*4294967296", "Number((BigInt(offsetLo) & 0xffffffffn) + (BigInt(offsetHi) * 0x100000000n))");
-  fs.writeFileSync(unrarFile, content);
-}
